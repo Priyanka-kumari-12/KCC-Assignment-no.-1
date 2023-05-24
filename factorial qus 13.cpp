c6b5d2ead8832f763bc52953696de5b5b310a297
Priyanka-kumari-12 <134132795+Priyanka-kumari-12@users.noreply.github.com>
2023-05-24 11:14:31 +0530

    Create factorial qus 13.cpp

---
+++ factorial qus 13.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+{
+    int main()
+    int number,factorial=1;
+    cin >> number;
+    for(  int i=number;i>=number;i--)
+    {
+
+        factorial=factorial*i;
+
+    }
+    cout<<"The factorial number is "<<factorial<< end1;
+    return 0;
+
+    
+}
